5ba920f8eb3029d4c5addf64ced6a468a9c50181
Skyler Hawthorne <skyler@dead10ck.com>
2021-10-13 16:47:27 -0400

    Fix wrong keymap

--- keyboards/moonlander/keymaps/dead10ck/keymap.c
+++ keyboards/moonlander/keymaps/dead10ck/keymap.c
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(2,KC_ESCAPE),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           MT(MOD_HYPR, KC_EQUAL),                                                                TO(1),          KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,       
     KC_LSHIFT,      LCTL_T(KC_Z),   LSFT_T(KC_X),   KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,      
     TD(DANCE_0),    TD(DANCE_1),    KC_LGUI,        OSL(5),         OSL(4),         LALT(KC_TAB),                                                                                                   TG(6),          KC_LGUI,        KC_RALT,        TD(DANCE_3),    TD(DANCE_4),    MO(3),          
-    LSFT_T(KC_BSPACE),LCTL_T(KC_ENTER),TD(DANCE_2),                    KC_LALT,        LCTL_T(KC_TAB), LSFT_T(KC_SPACE)
+    LCTL_T(KC_ENTER),LSFT_T(KC_TAB), TD(DANCE_2),                    KC_LALT,        LSFT_T(KC_BSPACE),LCTL_T(KC_SPACE)
   ),
   [1] = LAYOUT_moonlander(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
@@ -165,7 +165,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-extern bool g_suspend_state;
 extern rgb_config_t rgb_matrix_config;
 
 void keyboard_post_init_user(void) {
