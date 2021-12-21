6b05e79e0c437d9aa7016b1fb65c843025a1fd22
Skyler Hawthorne <skyler@dead10ck.com>
2021-12-21 16:55:19 -0500

    Split symbol / navigation layer
    * Get rid of Colemak (it was not meant to be)
    * Put navigation onto its own layer, accessible from former tmux layer
      OSL
    * Move tmux OSL to the left, make nested tmux a combo of the previous
      two
    * Rearrange symbol layer to make use of the right side, add OSL to
      previous GUI key, move GUI over to the right, make right alt a combo
      of the previous two

--- keyboards/moonlander/rules.mk
+++ keyboards/moonlander/rules.mk
@@ -12,6 +12,7 @@ MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
+COMBO_ENABLE   = yes        # Commands for debug and configuration
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
